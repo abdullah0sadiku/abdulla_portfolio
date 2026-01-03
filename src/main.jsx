ad64faa81f9e93e1c5516223a8dc5d4c497190c8
abdullah0sadiku <Avdullahsadiku02@gmail.com>
2026-01-04 00:15:03 +0100

    Add comment to force new build hash

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import './rtl.css'
 import App from './App.jsx'
 import './i18n'
 
+// Using BrowserRouter for clean URLs
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
